e69e1c59cd89621e3dea66cd34ef120fb0d7f874
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-28 12:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const input = document.getElementById('evaluatedText');
+const head = document.getElementById('letterCount');
+let count = 0;
+input.addEventListener('input' ,()=> {
+	++count;
+	head.innerText = count;
+	
+})
